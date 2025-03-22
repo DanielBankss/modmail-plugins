3525e3594bc56edd66141dcefedabd1f9081d441
Daniel <53154978+DanielBankss@users.noreply.github.com>
2025-03-22 13:10:16 +0000

    Update BetterMove.py

--- BetterMove/BetterMove.py
+++ BetterMove/BetterMove.py
@@ -13,24 +13,18 @@ class BetterMove(commands.Cog):
         }
         self.staff_role_ids = {
             "admins": "1299296154885951549",
-            "managers": "1299446515655249950",
             "mcstaff": "1214942778002505749",
             "dmods": "1214942755923427378"
         }
         
-
     @checks.thread_only()
     @commands.command()
     async def mc(self, ctx):        
-        category = discord.utils.get(ctx.guild.categories, id=1349813171459850293)
+        category = discord.utils.get(ctx.guild.categories, id=self.category_ids["mc"])
         roleId = self.staff_role_ids["mcstaff"]
         
-        if category is None or not isinstance(category, discord.CategoryChannel):
-            await ctx.send("Invalid category ID, Contact Dann.")
-            return
-        
         try:
-            await ctx.channel.edit(category=category)
+            await ctx.channel.edit(category=category, sync_permissions=True)
             await ctx.send(f"<@&{roleId}>")
         except Exception as e:
             await ctx.send(f"An error occurred: {str(e)}")
@@ -43,7 +37,7 @@ class BetterMove(commands.Cog):
         
         try:
             await ctx.channel.edit(category=category, sync_permissions=True)
-            # await ctx.send(f"<@&{roleId}>")
+            await ctx.send(f"<@&{roleId}>")
         except Exception as e:
             await ctx.send(f"An error occurred: {str(e)}")
         
@@ -53,12 +47,8 @@ class BetterMove(commands.Cog):
         category = get(ctx.guild.categories, id=self.category_ids["dmods"])
         roleId = self.staff_role_ids["dmods"]
         
-        if category is None or not isinstance(category, discord.CategoryChannel):
-            await ctx.send("Invalid category ID, Contact Dann.")
-            return
-        
         try:
-            await ctx.channel.edit(category=category)
+            await ctx.channel.edit(category=category, sync_permissions=True)
             await ctx.send(f"<@&{roleId}>")
         except Exception as e:
             await ctx.send(f"An error occurred: {str(e)}")
